d598183d0e09e302bed23444372deccbbaa5316d
Irving Leonard <irvingleonard@gmail.com>
2022-02-03 02:44:07 -0500

    fix(url): updated urls
    Using the new "path" function instead of the old "url"
    
    Removed outdated docstring

--- okta_client/urls.py
+++ okta_client/urls.py
@@ -1,19 +1,4 @@
-"""signer_django URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url, include
+from django.urls import path, include
 
 from . import apps
 from . import views
@@ -21,7 +6,7 @@ from . import views
 app_name = apps.OktaClientConfig.name
 
 urlpatterns = [
-	url(r'^saml/$', views.acs, name = 'acs'),
-	url(r'^login/$', views.login_, name = 'login'),
-	url(r'^logout/$', views.logout_, name = 'logout'),
+	path('saml/', views.acs, name = 'acs'),
+	path('login/', views.login_, name = 'login'),
+	path('logout/', views.logout_, name = 'logout'),
 ]
