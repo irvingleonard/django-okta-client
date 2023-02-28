3412738a8d688e577b51b61cc119bc1c1abe5238
Irving Leonard <irvingleonard@gmail.com>
2023-02-28 15:42:17 -0500

    Default dev deployment view is the dummy index one

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from okta_client import views
+
 urlpatterns = [
     path('accounts/', include('okta_client.urls')),
     path('admin/', admin.site.urls),
-    path('', admin.site.urls),
+    path('', views.index),
 ]
