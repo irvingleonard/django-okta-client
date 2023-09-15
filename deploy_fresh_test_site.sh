8b2a9853178e3167d41c87ecb91b1b9a0c8f8290
Irving Leonard <irvingleonard@gmail.com>
2023-09-15 18:48:32 -0400

    fix(build): updated local deployment script

--- deploy_fresh_test_site.sh
+++ deploy_fresh_test_site.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+DEFAULT_SUPERUSER_PASSWORD="My sup3r p4ssw0rd!"
+CURRENT_USER=`whoami`
+
 # Recreate virtual environment and install site dependencies
 rm -rfv ./venv
 (set -x; python3 -m venv venv)
 ./venv/bin/pip install --upgrade pip
-./venv/bin/pip install --upgrade setuptools wheel
-./venv/bin/pip install --upgrade django pysaml2
+./venv/bin/pip install --upgrade tomli
+./venv/bin/pip install --upgrade `./venv/bin/python -c "import tomli; tf = open('pyproject.toml', 'rb'); c = tomli.load(tf); print(' '.join(c['project']['dependencies']))"`
+./venv/bin/pip install --upgrade `./venv/bin/python -c "import tomli; tf = open('pyproject.toml', 'rb'); c = tomli.load(tf); print(' '.join(c['build-system']['requires']))"`
 
 # Create site structure
 rm -rfv ./test_site
@@ -19,16 +23,16 @@ rm -rfv ./test_site
 (set -x; ./venv/bin/python ./test_site/manage.py migrate --settings=test_site.local_settings)
 
 # Create super user
-(export DJANGO_SUPERUSER_LOGIN=`whoami`
+(export DJANGO_SUPERUSER_LOGIN=$CURRENT_USER
 export DJANGO_SUPERUSER_FIRSTNAME="$DJANGO_SUPERUSER_LOGIN"
 export DJANGO_SUPERUSER_LASTNAME="$DJANGO_SUPERUSER_LOGIN"
 export DJANGO_SUPERUSER_EMAIL="$DJANGO_SUPERUSER_LOGIN@invalid.local"
-export DJANGO_SUPERUSER_PASSWORD="My sup3r p4ssw0rd!"
-echo $DJANGO_SUPERUSER_PASSWORD | ./venv/bin/python ./test_site/manage.py createsuperuser --noinput --settings=test_site.local_settings)
+export DJANGO_SUPERUSER_PASSWORD=$DEFAULT_SUPERUSER_PASSWORD
+./venv/bin/python ./test_site/manage.py createsuperuser --noinput --settings=test_site.local_settings)
 
 echo "Now run with:
 
 env DJANGO_DEBUG=true ./venv/bin/python ./test_site/manage.py runserver --settings=test_site.local_settings
 
-Then go to http://localhost:8000/admin and use credentials $DJANGO_SUPERUSER_LOGIN:\"$DJANGO_SUPERUSER_PASSWORD\"
+Then go to http://localhost:8000/admin and use credentials $CURRENT_USER:$DEFAULT_SUPERUSER_PASSWORD
 "
