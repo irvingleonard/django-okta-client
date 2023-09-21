3db96e1d09cec4ed09a9ba1911507ab0e504fd87
Irving Leonard <irvingleonard@gmail.com>
2023-09-21 08:52:05 -0400

    feat(build): new version
    Also improved description of the project

--- okta_client/__init__.py
+++ okta_client/__init__.py
@@ -1,6 +1,10 @@
 #! python
 '''Okta client Django app
-This app adapts your Django site to work with Okta as it's idP
+This app adapts your Django site as a service provider to work with Okta SAML authentication.
+
+It defines a custom AUTH_USER_MODEL based on the Okta API User object and it has an optional authentication backend that connects to the Okta API and synchronizes the users and groups on login.
+
+There's also a mixin to handle Okta Event Hooks in your views easily.
 
 TODO:
 - Everything
@@ -9,4 +13,4 @@ Refs:
 - ?
 '''
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
