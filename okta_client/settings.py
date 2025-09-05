0932d456230b94dc7be67a9bc54da4378c4db6d3
Irving Leonard <irvingleonard@gmail.com>
2025-09-05 18:44:04 -0400

    Reworked settings

--- okta_client/settings.py
+++ okta_client/settings.py
@@ -3,18 +3,15 @@
 Okta Client Django settings.
 """
 
-from base64 import b64decode
+from logging import getLogger
 from urllib.parse import urlsplit, urlunsplit
-from warnings import warn
+
+from devautotools import path_for_setting
 
 DEFAULT_LOCAL_PATH = 'okta_client'
+LOGGER = getLogger(__name__)
 
 EXPECTED_VALUES_FROM_ENV = {
-	'OKTA_CLIENT_AUTH_SETTINGS_FROM_ENV': {
-		'OKTA_CLIENT_PRIVATE_KEY',
-		'OKTA_CLIENT_PRIVATE_KEY_BASE64',
-		'OKTA_CLIENT_TOKEN',
-	},
 	'OKTA_CLIENT_OAUTH_SETTINGS_FROM_ENV': {
 		'OKTA_CLIENT_ID',
 		'OKTA_CLIENT_SCOPES',
@@ -22,11 +19,17 @@ EXPECTED_VALUES_FROM_ENV = {
 	'OKTA_CLIENT_SETTINGS_FROM_ENV' : {
 		'OKTA_CLIENT_LOCAL_PATH',
 		'OKTA_CLIENT_ORG_URL',
+		'OKTA_CLIENT_PRIVATE_KEY',
+		'OKTA_CLIENT_TOKEN',
 		'OKTA_DJANGO_STAFF_USER_GROUPS',
 		'OKTA_DJANGO_SUPER_USER_GROUPS',
 		'OKTA_SAML_ASSERTION_DOMAIN_URL',
 		'OKTA_SAML_METADATA_AUTO_CONF_URL',
 	},
+}
+
+IMPLICIT_ENVIRONMENTAL_SETTINGS = {
+  'OKTA_CLIENT_LOCAL_PATH' : DEFAULT_LOCAL_PATH,
 }
 
 def normalized_settings(**django_settings):
@@ -55,18 +58,22 @@ def normalized_settings(**django_settings):
 	elif 'OKTA_CLIENT_ORG_URL' in django_settings['ENVIRONMENTAL_SETTINGS_KEYS']:
 		okta_client['ORG_URL'] = django_settings['ENVIRONMENTAL_SETTINGS']['OKTA_CLIENT_ORG_URL']
 
-	if EXPECTED_VALUES_FROM_ENV['OKTA_CLIENT_AUTH_SETTINGS_FROM_ENV'].intersection(django_settings['ENVIRONMENTAL_SETTINGS_KEYS']):
-		if 'OKTA_CLIENT_TOKEN' in django_settings['ENVIRONMENTAL_SETTINGS_KEYS']:
-			okta_client['API_TOKEN'] = django_settings['ENVIRONMENTAL_SETTINGS']['OKTA_CLIENT_TOKEN']
-		if EXPECTED_VALUES_FROM_ENV['OKTA_CLIENT_OAUTH_SETTINGS_FROM_ENV'].issubset(django_settings['ENVIRONMENTAL_SETTINGS_KEYS']):
-			okta_client['API_CLIENT_ID'] = django_settings['ENVIRONMENTAL_SETTINGS']['OKTA_CLIENT_ID']
-			okta_client['API_SCOPES'] = django_settings['ENVIRONMENTAL_SETTINGS']['OKTA_CLIENT_SCOPES']
-			if 'OKTA_CLIENT_PRIVATE_KEY' in django_settings['ENVIRONMENTAL_SETTINGS_KEYS']:
-				okta_client['API_PRIVATE_KEY'] = django_settings['ENVIRONMENTAL_SETTINGS']['OKTA_CLIENT_PRIVATE_KEY']
-			else:
-				okta_client['API_PRIVATE_KEY'] = b64decode(django_settings['ENVIRONMENTAL_SETTINGS']['OKTA_CLIENT_PRIVATE_KEY_BASE64'])
-		else:
-			warn('Not enough configuration provided for Okta OAuth', RuntimeWarning)
+	okta_api_client_key = path_for_setting(django_settings, 'OKTA_CLIENT_PRIVATE_KEY')
+	okta_api_client = None
+	if (EXPECTED_VALUES_FROM_ENV['OKTA_CLIENT_OAUTH_SETTINGS_FROM_ENV'].issubset(django_settings['ENVIRONMENTAL_SETTINGS_KEYS'])) and (okta_api_client_key is not None):
+		okta_api_client = {
+			'API_CLIENT_ID': django_settings['ENVIRONMENTAL_SETTINGS']['OKTA_CLIENT_ID'],
+			'API_SCOPES': django_settings['ENVIRONMENTAL_SETTINGS']['OKTA_CLIENT_SCOPES'],
+			'API_PRIVATE_KEY': okta_api_client_key
+		}
+	elif 'OKTA_CLIENT_TOKEN' in django_settings['ENVIRONMENTAL_SETTINGS_KEYS']:
+		okta_api_client = {'API_TOKEN': django_settings['ENVIRONMENTAL_SETTINGS']['OKTA_CLIENT_TOKEN']}
+	else:
+		LOGGER.debug('The Okta API client is not configured')
+
+	if okta_api_client is not None:
+
+		okta_client |= okta_api_client
 
 		if 'OKTA_DJANGO_SUPER_USER_GROUPS' in django_settings['ENVIRONMENTAL_SETTINGS_KEYS']:
 			okta_client['SUPER_USER_GROUPS'] = [group.strip() for group in django_settings['ENVIRONMENTAL_SETTINGS']['OKTA_DJANGO_SUPER_USER_GROUPS'].split(',')]
@@ -81,7 +88,7 @@ def normalized_settings(**django_settings):
 		elif 'okta_client.auth_backends.OktaBackend' not in django_settings['AUTHENTICATION_BACKENDS']:
 			django_settings['AUTHENTICATION_BACKENDS'] = ['okta_client.auth_backends.OktaBackend'] + django_settings['AUTHENTICATION_BACKENDS']
 
-	django_settings['OKTA_CLIENT_LOCAL_PATH'] = django_settings['ENVIRONMENTAL_SETTINGS'].get('OKTA_CLIENT_LOCAL_PATH', DEFAULT_LOCAL_PATH).strip('/')
+	django_settings['OKTA_CLIENT_LOCAL_PATH'] = django_settings['ENVIRONMENTAL_SETTINGS']['OKTA_CLIENT_LOCAL_PATH']
 
 	if 'REST_FRAMEWORK' not in django_settings:
 		django_settings['REST_FRAMEWORK'] = {
