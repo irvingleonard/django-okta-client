9d9059a76b059884f5ec88bcccd9229dd64a5565
Irving Leonard <irvingleonard@gmail.com>
2025-09-11 09:55:44 -0400

    auth_backends now leveraging the enhanced manager

--- okta_client/auth_backends.py
+++ okta_client/auth_backends.py
@@ -5,13 +5,10 @@ Okta authentication backend for Django.
 
 from logging import getLogger
 
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
 from rest_framework.authentication import TokenAuthentication as DjangoRESTTokenAuthentication
-
-from .api_client import OktaAPIClient
 
 LOGGER = getLogger(__name__)
 UserModel = get_user_model()
@@ -22,45 +19,18 @@ class OktaBackend(ModelBackend):
 	Include Okta related specifics to the authentication process.
 	"""
 
-	_api_client = OktaAPIClient()
-
-	def authenticate(self, request, login, **user_details):
+	def authenticate(self, request=None, username=None, login=None, **user_details):
 		"""Noop check
 		Retrieve the user details using the Okta API, check for admin access, and update group membership (creating groups if needed)
 		"""
 
-		try:
-			self._api_client.okta_api_client
-		except Exception as err:
-			okta_user = None
-			LOGGER.warning('Unable to use the Okta API client: %s', err)
-		else:
-			try:
-				okta_user = self._api_client('get_user', login)
-			except RuntimeError:
-				LOGGER.error('User not found in Okta: %s', login)
-				return None
+		if login is None:
+			return None
 
 		try:
-			user = UserModel.objects.get(pk=login)
-		except UserModel.DoesNotExist:
-			LOGGER.debug('Creating new local user: %s', login)
-			user = UserModel(login=login) if okta_user is None else UserModel.from_okta_user(okta_user)
-		else:
-			if okta_user is not None:
-				LOGGER.debug('Updating existing local user: %s', login)
-				user.update_from_okta_user(okta_user)
-		if user_details:
-			LOGGER.info('Updating user "%s" with values from the SAML assertion: %s', login, user_details)
-			user.update(**user_details)
-		user.is_active = True
-
-		user.save()
-
-		if okta_user is not None:
-			user.update_groups_from_okta()
-
-		return user
+			return UserModel.objects.get_user(login)
+		except ValueError:
+			return None
 
 
 class DjangoRESTBearerTokenAuthentication(DjangoRESTTokenAuthentication):
